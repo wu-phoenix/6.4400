5ae9209beeec49c4746d4fce2cf3accf46961d9c
Phoenix Wu <phoenixwu.personal@gmail.com>
2025-11-23 16:12:09 -0500

    starting hw5 again oops

---
+++ hw5/assignment5/build/CMakeFiles/assignment5.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/glad/src/glad.c" "CMakeFiles/assignment5.dir/external/src/glad/src/glad.c.o" "gcc" "CMakeFiles/assignment5.dir/external/src/glad/src/glad.c.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/assignment_code/assignment5/ShadowViewerApp.cpp" "CMakeFiles/assignment5.dir/assignment_code/assignment5/ShadowViewerApp.cpp.o" "gcc" "CMakeFiles/assignment5.dir/assignment_code/assignment5/ShadowViewerApp.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/assignment_code/assignment5/SunNode.cpp" "CMakeFiles/assignment5.dir/assignment_code/assignment5/SunNode.cpp.o" "gcc" "CMakeFiles/assignment5.dir/assignment_code/assignment5/SunNode.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/assignment_code/assignment5/main.cpp" "CMakeFiles/assignment5.dir/assignment_code/assignment5/main.cpp.o" "gcc" "CMakeFiles/assignment5.dir/assignment_code/assignment5/main.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/assignment_code/common/helpers.cpp" "CMakeFiles/assignment5.dir/assignment_code/common/helpers.cpp.o" "gcc" "CMakeFiles/assignment5.dir/assignment_code/common/helpers.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/examples/imgui_impl_glfw.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/examples/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/examples/imgui_impl_glfw.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/examples/imgui_impl_opengl3.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/examples/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/examples/imgui_impl_opengl3.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/imgui.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/imgui.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/imgui.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/imgui_demo.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_demo.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/imgui_draw.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_draw.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/external/src/imgui/imgui_widgets.cpp" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/assignment5.dir/external/src/imgui/imgui_widgets.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/Application.cpp" "CMakeFiles/assignment5.dir/gloo/Application.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/Application.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/Image.cpp" "CMakeFiles/assignment5.dir/gloo/Image.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/Image.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/InputManager.cpp" "CMakeFiles/assignment5.dir/gloo/InputManager.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/InputManager.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/MeshLoader.cpp" "CMakeFiles/assignment5.dir/gloo/MeshLoader.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/MeshLoader.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/Renderer.cpp" "CMakeFiles/assignment5.dir/gloo/Renderer.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/Renderer.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/Scene.cpp" "CMakeFiles/assignment5.dir/gloo/Scene.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/Scene.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/SceneNode.cpp" "CMakeFiles/assignment5.dir/gloo/SceneNode.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/SceneNode.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/Transform.cpp" "CMakeFiles/assignment5.dir/gloo/Transform.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/Transform.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/VertexObject.cpp" "CMakeFiles/assignment5.dir/gloo/VertexObject.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/VertexObject.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/cameras/ArcBallCameraNode.cpp" "CMakeFiles/assignment5.dir/gloo/cameras/ArcBallCameraNode.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/cameras/ArcBallCameraNode.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/cameras/BasicCameraNode.cpp" "CMakeFiles/assignment5.dir/gloo/cameras/BasicCameraNode.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/cameras/BasicCameraNode.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/components/CameraComponent.cpp" "CMakeFiles/assignment5.dir/gloo/components/CameraComponent.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/components/CameraComponent.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/components/RenderingComponent.cpp" "CMakeFiles/assignment5.dir/gloo/components/RenderingComponent.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/components/RenderingComponent.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/components/ShadingComponent.cpp" "CMakeFiles/assignment5.dir/gloo/components/ShadingComponent.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/components/ShadingComponent.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/debug/AxisNode.cpp" "CMakeFiles/assignment5.dir/gloo/debug/AxisNode.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/debug/AxisNode.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/debug/PrimitiveFactory.cpp" "CMakeFiles/assignment5.dir/gloo/debug/PrimitiveFactory.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/debug/PrimitiveFactory.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/gl_wrapper/BindGuard.cpp" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/BindGuard.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/BindGuard.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/gl_wrapper/BindableBuffer.cpp" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/BindableBuffer.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/BindableBuffer.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/gl_wrapper/Framebuffer.cpp" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/Framebuffer.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/Framebuffer.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/gl_wrapper/Texture.cpp" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/Texture.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/Texture.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/gl_wrapper/VertexArray.cpp" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/VertexArray.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/gl_wrapper/VertexArray.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/parsers/ObjParser.cpp" "CMakeFiles/assignment5.dir/gloo/parsers/ObjParser.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/parsers/ObjParser.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/shaders/PhongShader.cpp" "CMakeFiles/assignment5.dir/gloo/shaders/PhongShader.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/shaders/PhongShader.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/shaders/PlainTextureShader.cpp" "CMakeFiles/assignment5.dir/gloo/shaders/PlainTextureShader.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/shaders/PlainTextureShader.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/shaders/ShaderProgram.cpp" "CMakeFiles/assignment5.dir/gloo/shaders/ShaderProgram.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/shaders/ShaderProgram.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/shaders/SimpleShader.cpp" "CMakeFiles/assignment5.dir/gloo/shaders/SimpleShader.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/shaders/SimpleShader.cpp.o.d"
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/gloo/utils.cpp" "CMakeFiles/assignment5.dir/gloo/utils.cpp.o" "gcc" "CMakeFiles/assignment5.dir/gloo/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/phoenix/Documents/6.4400/hw5/assignment5/build/external/src/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw5/assignment5/build/CMakeFiles/assignment5.dir/gloo/components/ShadingComponent.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/assignment5.dir/gloo/components/ShadingComponent.cpp.o: \
+ /home/phoenix/Documents/6.4400/hw5/assignment5/gloo/components/ShadingComponent.cpp \
+ /usr/include/stdc-predef.h
